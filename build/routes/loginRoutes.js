dd35edd358afb0916b1e1e8177543d2e30aa66e2
oconely <and.ribaltovsky@gmail.com>
2020-03-12 23:56:48 +0300

    implemented user session

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -8,5 +8,15 @@ router.get('/login', function (req, res) {
 });
 router.post('/login', function (req, res) {
     var _a = req.body, email = _a.email, password = _a.password;
-    res.send(email + password);
+    if (email && password && email === 'hi@hi.com' && password === 'password') {
+        // mark this person as logged in
+        req.session = {
+            loggedIn: true
+        };
+        // redirect them to the root route
+        res.redirect('/');
+    }
+    else {
+        res.send('Invalid email or password');
+    }
 });
